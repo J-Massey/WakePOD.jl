9380731a00d72bb94ebae44004c63e012dd13abe
Jonah M <masseyjmo@gmail.com>
2023-06-15 18:19:39 +0200

    Got some stuff working

--- src/WakeDMD.py
+++ src/WakeDMD.py
@@ -25,7 +25,7 @@ vort2 = vort.reshape(nx * ny, nt)
 
 U, S, Vt = svd(vort1, full_matrices=False)
 # Now truncate the modes
-k=50
+k=150
 Ur, Sr, Vtr = U[:, :k], S[:k], Vt[:k, :]
 
 Atilde = (np.conj(Ur).T @ vort2) @ Vtr.T @ np.linalg.inv(np.diag(Sr))
@@ -33,14 +33,24 @@ Atilde = (np.conj(Ur).T @ vort2) @ Vtr.T @ np.linalg.inv(np.diag(Sr))
 W, D = np.linalg.eig(Atilde)
 lam = np.log(D)/dt
 
-# Plot the eigdenvalues
+# Plot the eigenvalues
 fig, ax = plt.subplots(figsize=(3, 3))
-ax.plot(lam.real, lam.imag, "o", ms=2)
+theta = np.linspace(0,1,200)*2*np.pi
+ax.plot(np.cos(theta)/2, np.sin(theta)/2, c='grey')
+ax.plot(np.diag(D).real, np.diag(D).imag, "P", markerfacecolor="none", ms=4, c='k', alpha=0.8)
 ax.set_xlabel(r"$\Re(\lambda)$")
 ax.set_ylabel(r"$\Im(\lambda)$")
 plt.savefig("figures/wakeDMD_eig.pdf", bbox_inches="tight")
 
-Phi = (vort2 @ Vtr.T) @ (np.linalg.inv(np.diag(Sr)) @ np.diag(W))
+print(D.shape)
+# Now we need to find the dominant frequencies
+eigenvalue_magnitudes = np.abs(np.diag(D))
+srt = np.flip(np.argsort(eigenvalue_magnitudes))
+frequencies = np.angle(np.diag(D)[srt])/(2 * np.pi)
+print(frequencies)
+
+# Sort by eigenvalue magnitude?
+Phi = vort2 @ (Vtr[srt, :].T @ np.linalg.inv(np.diag(Sr)[srt]) @ np.diag(W)[srt])
 print(Phi.shape)
 
 fig, ax = plt.subplots(figsize=(3, 3))
@@ -48,11 +58,10 @@ ax.plot(S / S.sum(), "o", ms=2)
 ax.set_xlabel("Mode number")
 ax.set_ylabel("Energy fraction")
 ax.set_yscale("log")
-plt.savefig("figures/wakePOD_energy.pdf", bbox_inches="tight")
+plt.savefig("figures/wakeDMD_energy.pdf", bbox_inches="tight")
 
 
-
-for m in range(0,k):
+for m in range(0,30):
     fig, ax = plt.subplots(figsize=(5, 3))
     lim = [-.0075, .0075]
     levels = np.linspace(lim[0], lim[1], 44)
@@ -69,6 +78,7 @@ for m in range(0,k):
         extend="both",
     )
     ax.set_aspect(1)
+    ax.set_title(f"$f^*={frequencies[m]/dt:.2f}$")
     plt.savefig(f"./figures/modes/Phi_{m}.png", dpi=600)
     plt.close()
 
