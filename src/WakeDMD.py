e97aeca9bed424a2cdd90a64466c8d5b9fc7e4ca
Jonah Iridis <masseyjmo@gmail.com>
2023-06-16 16:03:55 +0100

    Getting my head around the DMD and stability stuff

--- src/WakeDMD.py
+++ src/WakeDMD.py
@@ -1,10 +1,56 @@
 import numpy as np
-from scipy.linalg import qr, svd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import scienceplots
 plt.style.use(["science"])
 plt.rcParams["font.size"] = "10.5"
+
+def Sigma_plot(Sigma):
+    fig, ax = plt.subplots(figsize=(3, 3))
+    ax.plot(Sigma / Sigma.sum(), "o", ms=2)
+    ax.set_xlabel("Mode number")
+    ax.set_ylabel(r"$ \Sigma $")
+    ax.set_yscale("log")
+    plt.savefig("figures/Sigma.pdf", bbox_inches="tight")
+    plt.close()
+
+
+def lambdaPlot(Lambda):
+    # Plot the eigenvalues
+    fig, ax = plt.subplots(figsize=(3, 3))
+    # theta = np.linspace(0,1,200)*2*np.pi
+    # ax.plot(np.cos(theta)/2, np.sin(theta)/2, c='grey')
+    ax.plot(np.diag(Lambda).real, np.diag(Lambda).imag, "P", markerfacecolor="none", ms=4, c='k', alpha=0.8)
+    ax.set_xlabel(r"$\Re(\lambda)$")
+    ax.set_ylabel(r"$\Im(\lambda)$")
+    plt.savefig("figures/lambda.pdf", bbox_inches="tight")
+    plt.close()
+
+
+def omegaPlot(omega):
+    # Plot the eigenvalues
+    fig, ax = plt.subplots(figsize=(3, 3))
+    # theta = np.linspace(0,1,200)*2*np.pi
+    # ax.plot(np.cos(theta)/2, np.sin(theta)/2, c='grey')
+    ax.plot(np.diag(omega).imag, np.diag(omega).real, "o", markerfacecolor="none", ms=1, c='k', alpha=0.8)
+    ax.set_ylabel(r"$\Re(\omega)$")
+    ax.set_xlabel(r"$\Im(\omega)$")
+    plt.savefig("figures/omega.pdf", bbox_inches="tight")
+    plt.close()
+
+
+def bPlot(b):
+    # Plot the eigenvalues
+    fig, ax = plt.subplots(figsize=(3, 3))
+    # theta = np.linspace(0,1,200)*2*np.pi
+    # ax.plot(np.cos(theta)/2, np.sin(theta)/2, c='grey')
+    ax.plot(b, "X", markerfacecolor="none", ms=4, c='k', alpha=0.8)
+    # ax.set_xlabel(r"$\Re(\lambda)$")
+    # ax.set_ylabel(r"$\Im(\lambda)$")
+    plt.savefig("figures/ModeAmplitudes.pdf", bbox_inches="tight")
+    plt.close()
+
+
 
 xlims, ylims = (1, 2), (-0.35, 0.35)
 
@@ -21,47 +67,72 @@ pys = np.linspace(*ylims, ny)
 
 # Flatten snapshots along first two dimensions
 vort1 = vort.reshape(nx * ny, nt)
-vort2 = vort.reshape(nx * ny, nt)
+vort2 = vort2.reshape(nx * ny, nt)
 
-U, S, Vt = svd(vort1, full_matrices=False)
-# Now truncate the modes
-k=150
-Ur, Sr, Vtr = U[:, :k], S[:k], Vt[:k, :]
+k = 200
+# def DMD(X,Xprime,r):
+U,Sigma,VT = np.linalg.svd(vort1,full_matrices=False) # Step 1 - Heaviest
+Sigma_plot(Sigma)
+Ur = U[:,:k]
+Sigmar = np.diag(Sigma[:k])
+VTr = VT[:k,:]
+Atilde = np.linalg.solve(Sigmar.T,(Ur.T @ vort2 @ VTr.T).T).T # Step 2 - Find the linear operator using psuedo inverse
+# Now backwards
+U,Sigma,VT = np.linalg.svd(vort2,full_matrices=False)
+Sigma_plot(Sigma)
+Ur = U[:,:k]
+Sigmar = np.diag(Sigma[:k])
+VTr = VT[:k,:]
+Atildeb = np.linalg.solve(Sigmar.T,(Ur.T @ vort1 @ VTr.T).T).T
 
-Atilde = (np.conj(Ur).T @ vort2) @ Vtr.T @ np.linalg.inv(np.diag(Sr))
+Atilde = 1/2*(Atilde + np.linalg.inv(Atildeb))
 
-W, D = np.linalg.eig(Atilde)
-lam = np.log(D)/dt
+Lambda, W = np.linalg.eig(Atilde) # Step 3 - Eigenvalues
+Lambda = np.diag(Lambda)
+lambdaPlot(Lambda)
+omega = np.log(Lambda)/dt  # Spectral expansion
+omegaPlot(omega)
 
-# Plot the eigenvalues
+# Now sort everything based on the real part of the spectral expansion
+p = np.argsort(omega.imag)
+Atildes = Atilde[p]
+Sigmars = Sigmar[p]
+Ws = W[p]
+VTrs = VTr[p]
+Lambdas = Lambda[p]
+
+
+Phi = vort2 @ np.linalg.solve(Sigmar.T,VTr).T @ W # Step 4 - Modes
+alpha1 = Sigmar @ VTr[:,0]  # Time?
+b = np.linalg.solve(W @ Lambda,alpha1)  # The mode amplitudes
+    # return Phi, Lambda, b
+bPlot(b)
+print(VTr.shape)
+
+
 fig, ax = plt.subplots(figsize=(3, 3))
-theta = np.linspace(0,1,200)*2*np.pi
-ax.plot(np.cos(theta)/2, np.sin(theta)/2, c='grey')
-ax.plot(np.diag(D).real, np.diag(D).imag, "P", markerfacecolor="none", ms=4, c='k', alpha=0.8)
-ax.set_xlabel(r"$\Re(\lambda)$")
-ax.set_ylabel(r"$\Im(\lambda)$")
-plt.savefig("figures/wakeDMD_eig.pdf", bbox_inches="tight")
-
-print(D.shape)
-# Now we need to find the dominant frequencies
-eigenvalue_magnitudes = np.abs(np.diag(D))
-srt = np.flip(np.argsort(eigenvalue_magnitudes))
-frequencies = np.angle(np.diag(D)[srt])/(2 * np.pi)
-print(frequencies)
-
-# Sort by eigenvalue magnitude?
-Phi = vort2 @ (Vtr[srt, :].T @ np.linalg.inv(np.diag(Sr)[srt]) @ np.diag(W)[srt])
-print(Phi.shape)
-
-fig, ax = plt.subplots(figsize=(3, 3))
-ax.plot(S / S.sum(), "o", ms=2)
-ax.set_xlabel("Mode number")
-ax.set_ylabel("Energy fraction")
-ax.set_yscale("log")
-plt.savefig("figures/wakeDMD_energy.pdf", bbox_inches="tight")
+ax.plot(alpha1, "X", markerfacecolor="none", ms=4, c='k', alpha=0.8)
+# ax.set_xlabel(r"$\Re(\lambda)$")
+# ax.set_ylabel(r"$\Im(\lambda)$")
+plt.savefig("figures/freqs.pdf", bbox_inches="tight")
+plt.close()
 
 
-for m in range(0,30):
+for m in range(0,k):
+    fig, ax = plt.subplots(figsize=(3, 3))
+    # theta = np.linspace(0,1,200)*2*np.pi
+    # ax.plot(np.cos(theta)/2, np.sin(theta)/2, c='grey')
+    ax.plot(np.linspace(0,8,nt-1), VTr[m, :], c='k', alpha=0.8)
+    ax.set_xlabel(r"$t$")
+    # ax.set_ylabel(r"$\Im(\omega)$")
+    plt.savefig(f"figures/TimeDynamics/V_{m}.png", bbox_inches="tight")
+    plt.close()
+
+
+print(Lambda.shape)
+
+
+for m in range(0,k):
     fig, ax = plt.subplots(figsize=(5, 3))
     lim = [-.0075, .0075]
     levels = np.linspace(lim[0], lim[1], 44)
@@ -78,7 +149,7 @@ for m in range(0,30):
         extend="both",
     )
     ax.set_aspect(1)
-    ax.set_title(f"$f^*={frequencies[m]/dt:.2f}$")
+    # ax.set_title(f"$f^*={frequencies[m]/dt:.2f}$")
     plt.savefig(f"./figures/modes/Phi_{m}.png", dpi=600)
     plt.close()
 
