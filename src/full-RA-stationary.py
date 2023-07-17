010be8640772c93795d580aecc271d48fd84ac23
Jonah Iridis <masseyjmo@gmail.com>
2023-07-17 16:40:46 +0100

    Redi=oing LAP y

--- src/full-RA-stationary.py
+++ src/full-RA-stationary.py
@@ -19,13 +19,13 @@ plt.rcParams["font.size"] = "10.5"
 
 u = np.load("data/stationary/10k/u.npy")
 u = np.einsum("ijk -> kji", u)
-u = u[::16, ::16, ::64]
+u = u[::4, ::4, ::8]
 v = np.load("data/stationary/10k/v.npy")
 v = np.einsum("ijk -> kji", v)
-v = v[::16, ::16, ::64]
+v = v[::4, ::4, ::8]
 p = np.load("data/stationary/10k/p.npy")
 p = np.einsum("ijk -> kji", p)
-p = p[::16, ::16, ::64]
+p = p[::4, ::4, ::8]
 
 xlims, ylims = (-0.35, 2), (-0.35, 0.35)
 nx, ny, nt = v.shape
@@ -55,7 +55,7 @@ print("Flucs done")
 means = np.array([u_mean, v_mean, p_mean])
 flucs_field = np.array([u_flucs, v_flucs, p_flucs])
 flat_mean_field = means.reshape(3, nx * ny)
-flat_fucks = flucs_field.reshape(3, nx * ny, nt)
+flat_flucs = flucs_field.reshape(3, nx * ny, nt)
 
 
 def create_grad_operator_y(nx, ny, dy):
@@ -213,7 +213,7 @@ D1x = create_grad_operator_x(nx, ny, dx)
 D1y = create_grad_operator_y(nx, ny, dy)
 # Retest the gradient operators
 np.isclose(
-    (D1y @ flat_fucks[0]).reshape(nx, ny, nt),
+    (D1y @ flat_flucs[0]).reshape(nx, ny, nt),
     np.gradient(flucs_field[0], dy, axis=1, edge_order=2),
 )
 
@@ -244,7 +244,7 @@ L3 = np.array([D1x.toarray(), D1x.toarray(), Z])  # continuity x
 L4 = np.array([D1y.toarray(), D1y.toarray(), Z])  # continuity y
 L = np.array([L1, L2, L3, L4])
 print("The memory size of numpy array arr is:", L.itemsize * L.size / 1e9, "GB")
-Lq = (L @ flat_fucks).sum(
+Lq = (L @ flat_flucs).sum(
     axis=0
 )  # Should't have to sum this for a matrix operation but looks alright
 
@@ -272,23 +272,26 @@ L4_big = np.concatenate((D1y.toarray(), D1y.toarray(), Z), axis=1)
 
 L1_big.shape
 L_big = np.concatenate((L1_big, L2_big, L3_big, L4_big), axis=0)
-big_fucks = flat_fucks.reshape(3 * nx * ny, nt).repeat(4, axis=0)
-big_fucks.shape
-(L1_big @ big_fucks)
+L_big.shape
+big_flucs = flat_flucs.reshape(3 * nx * ny, nt)#.repeat(3, axis=0)
+big_flucs.shape
+Lq = (L_big @ big_flucs)
 
 # Test the product with the discrete operator
+# Reshape Lq
+Lq_reshaped = (Lq.reshape(3, nx*ny, nt)).reshape(nx, ny, nt)
 
 lim = [-10, 10]
 fig, ax = plt.subplots(figsize=(5, 4))
 levels = np.linspace(lim[0], lim[1], 44)
 _cmap = sns.color_palette("seismic", as_cmap=True)
 
-te_field = Lq[0, :, :].reshape(nx, ny, nt)
+te_field = Lq_reshaped[0, :, :, :]
 
 cont = ax.contourf(
     pxs,
     pys,
-    te_field[:, :, 5].T,
+    te_field[:, :, 6].T,
     levels=levels,
     vmin=lim[0],
     vmax=lim[1],
@@ -345,96 +348,9 @@ anim = animation.FuncAnimation(
     fig, animate, frames=nt, interval=30, blit=True, repeat=False
 )
 
-anim.save(f"stationary/figures/test_p.gif", fps=50, bitrate=-1, dpi=400)
+anim.save(f"stationary/figures/test_p.gif", fps=5, bitrate=-1, dpi=400)
 
 
-Luprime1 = (L1[0] @ flat_fucks[0, :, 0] + L1[1] @ flat_fucks[1, :, 0]).reshape(nx, ny)
-np.isclose(Luprime1, Luprime[0, :, :, 0])
-
-flat_fucks.shape
-
-
-def compute_laplacian(q, dx, dy):
-    # Extracting the dimensions
-    dim = 2
-
-    # Computing the Laplacian
-    laplacian = np.empty_like(q)
-
-    for d in range(dim):
-        laplacian[d] = np.gradient(
-            np.gradient(q[d], dx, axis=0, edge_order=2), dx, axis=0, edge_order=2
-        ) + np.gradient(
-            np.gradient(q[d], dy, axis=1, edge_order=2), dy, axis=1, edge_order=2
-        )
-
-    return laplacian
-
-
-# This is
-compute_laplacian(flucs_field, dx, dy)[0]
-(LAP @ flat_fucks[0]).reshape(nx, ny, nt)
-
-gop = (D1x @ (D1x @ flat_fucks[0]) + D1y @ (D1y @ flat_fucks[0])).reshape(nx, ny, nt)
-np.isclose(compute_laplacian(flucs_field, dx, dy)[0], gop)
-
-test = np.random.rand(nx, ny, nt)
-d2x = np.gradient(
-    np.gradient(flucs_field[0], dx, axis=0, edge_order=2), dx, axis=0, edge_order=2
-)
-d2y = np.gradient(
-    np.gradient(flucs_field[0], dy, axis=1, edge_order=2), dy, axis=1, edge_order=2
-)
-od2x = (D1x @ (D1x @ flat_fucks[0])).reshape(nx, ny, nt)
-od2y = (D1y @ (D1y @ flat_fucks[0])).reshape(nx, ny, nt)
-np.isclose(od2y, d2y)
-
-d1y = np.gradient(flucs_field[0], dy, axis=1, edge_order=2)
-
-
-def grad(q, dx, dy):
-    # Extracting the dimensions
-    dim = 2
-    dd = [dx, dy]
-    # Computing the Laplacian
-    grad = np.empty_like(q)
-
-    for d in range(dim):
-        grad[d] = np.gradient(q[d], dd[d], axis=d, edge_order=2)
-
-    return grad
-
-
-def lns_operator(ubar, q, Re):
-    # Extracting the dimensions
-    dim, nt, ny, nx = q.shape
-
-    dot_product1 = -(np.flip(ubar, axis=0) * grad(q, dx, dy)).sum(axis=0)
-    dot_product2 = -(np.flip(q, axis=0) * grad(ubar, dx, dy)).sum(axis=0)
-
-    # Compute the Laplacian of u_bar as the second derivatives
-    laplacian = compute_laplacian(q, dx, dy)
-    operator = np.empty_like(q)
-    for d in range(dim):
-        operator[d] = dot_product1 + dot_product2 + 1 / Re * laplacian[d]
-    return operator
-
-
-Luprime = lns_operator(
-    np.repeat(means.reshape(2, nx, ny, 1), nt, axis=3), flucs_field, 10250
-)
-# Luprime = np.load("stationary/Luprime.npy")
-
-
-# np.save("data/stationary/10k/L.npy", L)
-# # L = np.load("data/stationary/10k/L.npy")
-# dim, nx, ny, fNt = L.shape
-# Lflat = L.reshape(2, nx*ny, fNt)
-
-
-# # np.save("data/stationary/10k/fft_velocity.npy", fft_flucs)
-# # Lflatfftu = fft(Lflat[0].T)
-# # Lflatfftv = fft(Lflat[1].T)
 
 # omegaspan = np.linspace(1,20*np.pi)
 # Sigmau, Sigmav = [], []
