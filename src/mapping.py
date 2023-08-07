af566a5b0e046e4d0a6cb4c2c0d98ae1bacffbb0
Jonah Iridis <masseyjmo@gmail.com>
2023-08-07 12:53:55 +0100

    Bloody working

--- src/mapping.py
+++ src/mapping.py
@@ -6,17 +6,41 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import scienceplots
 import matplotlib.animation as animation
-plt.style.use(["science"])
+plt.style.use(["science", "grid"])
 plt.rcParams["font.size"] = "10.5"
 plt.rcParams["image.cmap"] = "gist_earth"
 
-fp = "data/swimming/10k"
 
+def plot_flows(qi, fn, _cmap, lim, fwarp):
+    # Test plot
+    fig, ax = plt.subplots(figsize=(5, 3))
+    levels = np.linspace(lim[0], lim[1], 44)
+    _cmap = sns.color_palette(_cmap, as_cmap=True)
+    cs = ax.contourf(
+        pxs,
+        pys,
+        qi,
+        levels=levels,
+        vmin=lim[0],
+        vmax=lim[1],
+        # norm=norm,
+        cmap=_cmap,
+        extend="both",
+        alpha=0.7
+    )
+    ax.set_aspect(1)
+    # ax.set_title(f"$\omega={frequencies_bsort[oms]:.2f},St={frequencies_bsort[oms]/(2*np.pi):.2f}$")
+    ax.plot(pxs, fwarp(pxs), c='k')
+    plt.savefig(f"./swimming/figures/{fn}.pdf", dpi=600)
+    plt.close()
+
+
+fp = "data/swimming/10k"
 
 def load_and_process_data(filepath):
     data = np.load(filepath)
     data = np.einsum("ijk -> kji", data)
-    return data[::8, ::8, ::16]
+    return data[::4, ::4, ::50]
 
 
 u = load_and_process_data(f"{fp}/u.npy")
@@ -39,18 +63,21 @@ def bodify(arr, pxs):
 
 
 utest = bodify(u, pxs)
-utest = utest[:, :, 0]
-xlims, ylims = (0, 1), (-0.35, 0.35)
 nx, ny, nt = utest.shape
+utest = utest[:, :, -1]
+xlims, ylims = (0, 1), (-0.35, 0.35)
 pxs = np.linspace(*xlims, nx)
 pys = np.linspace(*ylims, ny)
 
 
-def warp_velocity_field(u, pxs, pys):
+def fwarp(pxs):
+    return -0.5*(0.28 * pxs**2 - 0.13 * pxs + 0.05) * np.sin(2*np.pi*(0 - (1.42 * pxs)))
+
+def warp_velocity_field(u, pxs):
     # Calculate the shifts for each x-coordinate
-    fwarp = 0.05 * pxs**2
+    fw = fwarp(pxs)
     dy = (- ylims[0] + ylims[1])/ny
-    shifts = np.round(fwarp/dy).astype(int)
+    shifts = np.round(fw/dy).astype(int)
 
     # Create a new velocity field filled with NaNs
     new_u_shifted = np.full(u.shape, np.nan)
@@ -66,26 +93,7 @@ def warp_velocity_field(u, pxs, pys):
     
     return new_u_shifted
 
+new_u_shifted = warp_velocity_field(utest, pxs)
 
-def plot_flows(qi, fn, _cmap, lim):
-    # Test plot
-    fig, ax = plt.subplots(figsize=(5, 3))
-    levels = np.linspace(lim[0], lim[1], 44)
-    _cmap = sns.color_palette(_cmap, as_cmap=True)
-    cs = ax.contourf(
-        pxs,
-        pys,
-        qi,
-        levels=levels,
-        vmin=lim[0],
-        vmax=lim[1],
-        # norm=norm,
-        cmap=_cmap,
-        extend="both",
-    )
-    ax.set_aspect(1)
-    # ax.set_title(f"$\omega={frequencies_bsort[oms]:.2f},St={frequencies_bsort[oms]/(2*np.pi):.2f}$")
-    plt.savefig(f"./swimming/figures/{fn}.pdf", dpi=600)
-    plt.close()
 
-plot_flows(new_u_shifted.T, "warp_test", "icefire_r", [0., 1.4])
+plot_flows(new_u_shifted.T, "warp_test", "icefire_r", [0., 1.4], fwarp)
