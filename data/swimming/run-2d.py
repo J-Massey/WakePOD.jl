d70aa5943f193a2177e6f5ca55120b181b6358fe
Jonah Iridis <masseyjmo@gmail.com>
2023-08-10 14:33:33 +0100

    ArXiving

--- data/swimming/run-2d.py
+++ data/swimming/run-2d.py
@@ -1,14 +1,1 @@
-from lotus import run
-from pathlib import Path
-import time
-
-
-def run2d():
-    run(256, f'{cwd}/lotus-data')
-
-if __name__ == "__main__":
-    cwd = Path.cwd()
-    run2d()
-
-    
-
+../../../BumpStab/data/0/run-2d.py
