9f068e35f5e8fccdc46b1170c649e6e9fb7d3fe0
Jonah Iridis <masseyjmo@gmail.com>
2023-08-10 08:15:33 +0100

    Shifting to new repo, archiving as sketch project

--- src/unwarp.py
+++ src/unwarp.py
@@ -140,51 +140,50 @@ Uf, Sigmaf, VTf = svd(fluc1, full_matrices=False)
 
 print("SVDone")
 
-rs = [nt-3, nt-2, nt-1] # input("Enter the number of DMD modes you'd like to retain (e.g., 2): ")
-for r in rs:
+r = nt - 2 # input("Enter the number of DMD modes you'd like to retain (e.g., 2): ")
 # Sigma_plot(Sigma)
-    U_r = Ub[:,:r]
-    Sigmar = np.diag(Sigmab[:r])
-    VT_r = VTb[:r,:]
-    Atildeb = np.linalg.solve(Sigmar.T,(U_r.T @ fluc1 @ VT_r.T).T).T
+U_r = Ub[:,:r]
+Sigmar = np.diag(Sigmab[:r])
+VT_r = VTb[:r,:]
+Atildeb = np.linalg.solve(Sigmar.T,(U_r.T @ fluc1 @ VT_r.T).T).T
 
 
-    U_r = Uf[:, :r]
-    S_r = np.diag(Sigmaf[:r])
-    VT_r = VTf[:r, :]
-    Atildef = np.linalg.solve(S_r.T,(U_r.T @ fluc2 @ VT_r.T).T).T # Step 2 - Find the linear operator using psuedo inverse
+U_r = Uf[:, :r]
+S_r = np.diag(Sigmaf[:r])
+VT_r = VTf[:r, :]
+Atildef = np.linalg.solve(S_r.T,(U_r.T @ fluc2 @ VT_r.T).T).T # Step 2 - Find the linear operator using psuedo inverse
 
-    # Find the linear operator
-    A_tilde = 1/2*(Atildef + np.linalg.inv(Atildeb))
-    rho, W = np.linalg.eig(A_tilde)
+# Find the linear operator
+A_tilde = 1/2*(Atildef + np.linalg.inv(Atildeb))
+rho, W = np.linalg.eig(A_tilde)
 
-    # Find the eigenfunction from spectral expansion
-    Lambda = np.log(rho)/dt
+# Find the eigenfunction from spectral expansion
+Lambda = np.log(rho)/dt
 
-    # Find the DMD modes
-    V_r = np.dot(np.dot(fluc2, VT_r.T), np.dot(np.linalg.inv(S_r), W))
+# Find the DMD modes
+V_r = np.dot(np.dot(fluc2, VT_r.T), np.dot(np.linalg.inv(S_r), W))
 
-    # Find the hermatian adjoint of the 
-    V_r_star_Q = V_r.conj().T
-    V_r_star_Q_V_r = np.dot(V_r_star_Q, V_r)
-    # Cholesky factorization
-    F_tilde = cholesky(V_r_star_Q_V_r)
+# Find the hermatian adjoint of the 
+V_r_star_Q = V_r.conj().T
+V_r_star_Q_V_r = np.dot(V_r_star_Q, V_r)
+# Cholesky factorization
+F_tilde = cholesky(V_r_star_Q_V_r)
 
-    omegaSpan = np.linspace(0, 1000, 2000)
-    gain = np.empty((omegaSpan.size, Lambda.size))
-    for idx, omega in tqdm(enumerate(omegaSpan)):
-        R = np.linalg.svd(F_tilde@np.linalg.inv((-1j*omega)*np.eye(Lambda.shape[0])-np.diag(Lambda))@np.linalg.inv(F_tilde),
-                        compute_uv=False)
-        gain[idx] = R**2
+omegaSpan = np.linspace(0, 500, 500)
+gain = np.empty((omegaSpan.size, Lambda.size))
+for idx, omega in tqdm(enumerate(omegaSpan)):
+    R = np.linalg.svd(F_tilde@np.linalg.inv((-1j*omega)*np.eye(Lambda.shape[0])-np.diag(Lambda))@np.linalg.inv(F_tilde),
+                    compute_uv=False)
+    gain[idx] = R**2
 
-    fig, ax = plt.subplots(figsize = (3,3))
-    ax.set_xlabel(r"$\omega$")
-    ax.set_ylabel(r"$\sigma_i$")
-    # ax.set_xlim(0, 10)
-    for i in range(0,min(r,4)):
-        ax.loglog(omegaSpan, np.sqrt(gain[:, i]))
-    plt.savefig(f"{case}/figures/opt_gain_DMD_{r}.png", dpi=700)
-    plt.close()
+fig, ax = plt.subplots(figsize = (3,3))
+ax.set_xlabel(r"$\omega$")
+ax.set_ylabel(r"$\sigma_i$")
+# ax.set_xlim(0, 10)
+for i in range(0,min(r,4)):
+    ax.loglog(omegaSpan, np.sqrt(gain[:, i]))
+plt.savefig(f"{case}/figures/opt_gain_DMD_{r}.png", dpi=700)
+plt.close()
 
 
 # Find peaks in the gain data
